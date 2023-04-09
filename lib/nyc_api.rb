5800d264c19a9d456b0ab01834de835f63f6e849
Hannan <najma.mahat@student.moringaschool.com>
2023-04-09 18:00:48 +0300

    Working with API

--- lib/nyc_api.rb
+++ lib/nyc_api.rb
@@ -1,7 +1,7 @@
-require 'net/http'
-require 'open-uri'
-require 'json'
- 
+require "net/http"
+require "open-uri"
+require "json"
+
 class GetPrograms
 
   URL = "http://data.cityofnewyork.us/resource/uvks-tn5n.json"
@@ -12,7 +12,14 @@ class GetPrograms
     response.body
   end
 
+  def program_school
+    
+    # we use the JSON library to parse the API response into nicely formatted JSON
+    programs = JSON.parse(self.get_programs)
+    programs.collect { |program| program["agency"] }
+  end
 end
 
-programs = GetPrograms.new.get_programs
-puts programs
+# programs = GetPrograms.new.get_programs
+programs = GetPrograms.new
+puts programs.program_school.uniq
